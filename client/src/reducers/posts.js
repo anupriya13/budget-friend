9ff74c41c56c3c98f6d2b79c319b5faec450d150
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2021-02-13 21:56:47 +0530

    Add files via upload

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,21 @@
+import {FETCH_ALL , DELETE, UPDATE, CREATE} from '../constants/actionTypes';
+
+export default (posts = [], action) => {
+  switch (action.type) {
+
+    case UPDATE:
+      return posts.map((post) => post._id == action.payload._id ? action.payload : post);
+
+    case FETCH_ALL:
+      return action.payload;
+    
+    case CREATE:
+      return [...posts, action.payload];
+    
+    case DELETE:
+        return posts.filter((post) => post._id !== action.payload);
+
+    default:
+      return posts;
+  }
+};
